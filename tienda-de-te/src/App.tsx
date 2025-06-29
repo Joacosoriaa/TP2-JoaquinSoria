62daa01ae0efaad45b8ccde77be45f42cefc6af6
Joacosoriaa <joacosoria.js@gmail.com>
2025-06-29 19:51:32 -0300

    Creacion de estructura y archivos

---
+++ tienda-de-te/src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Tienda from './pages/Tienda';
+import Tutoriales from './pages/Tutoriales';
+import MenuTienda from './pages/MenuTienda';
+import TrabajaConNosotros from './pages/TrabajaConNosotros';
+import Franquicias from './pages/Franquicias';
+import Contacto from './pages/Contacto';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />;
+      case 'tienda':
+        return <Tienda />;
+      case 'tutoriales':
+        return <Tutoriales />;
+      case 'menu-tienda':
+        return <MenuTienda />;
+      case 'trabaja-con-nosotros':
+        return <TrabajaConNosotros />;
+      case 'franquicias':
+        return <Franquicias />;
+      case 'contacto':
+        return <Contacto />;
+      default:
+        return <Home />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <main className="pt-16">
+        {renderPage()}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
